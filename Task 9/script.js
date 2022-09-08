483a2230314e0ebd67861670dcac9322e3bdd5f9
unknown <justinasve@gmail.com>
2022-09-09 00:48:20 +0300

    ninth task done

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,25 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+    constructor(title, director, budget) {
+        this.title = title,
+        this.director = director,
+        this.budget = budget
+    }
+    
+    wasExpensive() {
+        // ar galima su ternary operator returninti? Nepavyko vietoj if-else panaudoti šitoje vietoje
+        if (this.budget > 100000000) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
+
+const myMovie = new Movie('Home Alone', 'Chris Columbus', 18000000);
+
+console.log(myMovie.wasExpensive());
+
