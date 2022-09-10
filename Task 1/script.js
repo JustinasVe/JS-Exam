393e1cd54ce5a5209c2dcf11817624d5437dbbf6
unknown <justinasve@gmail.com>
2022-09-11 02:31:39 +0300

    first task done

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,26 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+const formElement = document.querySelector('form');
+const userInput = document.querySelector('#search');
+const outputToUser = document.querySelector('#output');
+
+userInput.setAttribute('type', 'number');
+
+formElement.addEventListener('submit', (event) => {
+    let svarai = userInput.value * 2.2046;
+    let gramai = userInput.value / 0.0010000;
+    let uncijos = userInput.value * 35.274;
+    let divSvarai = document.createElement('div');
+    let divGramai = document.createElement('div');
+    let divUncijos = document.createElement('div');
+    divGramai.textContent = `Gramai (g): ${gramai}`;
+    divSvarai.textContent = `Svarai (lb): ${svarai}`;
+    divUncijos.textContent = `Uncijos (oz): ${uncijos}`;
+    outputToUser.appendChild(divSvarai);
+    outputToUser.appendChild(divGramai);
+    outputToUser.appendChild(divUncijos);
+    event.preventDefault();
+});
+
